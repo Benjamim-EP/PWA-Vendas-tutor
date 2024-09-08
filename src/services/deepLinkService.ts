2ede09e41c7d87c86b2e986e68a0a9f97ca90f47
Benjamim-EP <bannk18@gmail.com>
2024-09-08 12:09:46 -0300

    atualização deeplink olx

--- src/services/deepLinkService.ts
+++ src/services/deepLinkService.ts
@@ -3,5 +3,5 @@ import { IDeepLink } from '../interfaces/IDeepLink';
 // Serviço responsável por definir os links de deep link
 export const deepLinkService: IDeepLink[] = [
   { name: 'Facebook', url: 'fb://' },
-  { name: 'OLX', url: 'olx://' },
+  { name: 'OLX', url: 'olxapp://' },
 ];
